3ad09be529c6350d27496f7d907e23c37bfc98f8
octorock <79596758+octorock@users.noreply.github.com>
2021-10-26 08:08:05 +0200

    Add names for local flags

--- src/explorer.js
+++ src/explorer.js
@@ -1,4 +1,4 @@
-import {enemies, npcs, objects, globalFlags, projectiles} from './enums.js';
+import { enemies, npcs, objects, globalFlags, projectiles, localFlagsOutdoors, localFlagsIndoors, localFlagsCaves1, localFlagsCaves2, localFlagsDWS, localFlagsCOF, localFlagsFOW, localFlagsTOD, localFlagsPOW, localFlagsDHC } from './enums.js';
 
 let nameByKind = {
     '1': 'PLAYER',
@@ -80,7 +80,7 @@ function getHumanName(entity) {
 
 function selectEntity(listIndex, entityIndex) {
     if (selectedEntityIndex != -1) {
-        let div = document.getElementById('entity_'+selectedListIndex+'_'+selectedEntityIndex);
+        let div = document.getElementById('entity_' + selectedListIndex + '_' + selectedEntityIndex);
         if (div) {
             div.classList.remove('selected');
         }
@@ -88,7 +88,7 @@ function selectEntity(listIndex, entityIndex) {
     selectedListIndex = listIndex;
     selectedEntityIndex = entityIndex;
     if (selectedEntityIndex != -1) {
-        let div = document.getElementById('entity_'+selectedListIndex+'_'+selectedEntityIndex);
+        let div = document.getElementById('entity_' + selectedListIndex + '_' + selectedEntityIndex);
         if (div) {
             div.classList.add('selected');
         }
@@ -104,11 +104,11 @@ function selectEntity(listIndex, entityIndex) {
             let script = -2;
             if (entity.cutsceneBeh.addr == 0x2022750) { // gPlayerScriptExecutionContext
                 script = -1;
-            } else if(entity.cutsceneBeh.addr >= 0x2036570 && entity.cutsceneBeh.addr <0x2036570+0x20*36) { // gScriptExecutionContextArray
-                script = (entity.cutsceneBeh.addr-0x2036570) / 36
+            } else if (entity.cutsceneBeh.addr >= 0x2036570 && entity.cutsceneBeh.addr < 0x2036570 + 0x20 * 36) { // gScriptExecutionContextArray
+                script = (entity.cutsceneBeh.addr - 0x2036570) / 36
             }
             if (script != -2) {
-                description = '<a href="javascript:showScript('+script+')">Show script execution context</a><br/>'
+                description = '<a href="javascript:showScript(' + script + ')">Show script execution context</a><br/>'
             }
         }
 
@@ -119,11 +119,11 @@ function selectEntity(listIndex, entityIndex) {
     document.getElementById('details').innerHTML = description;
 }
 
-window.showScript = function(id) {
+window.showScript = function (id) {
     if (id == -1) {
         showState('gPlayerScriptExecutionContext', currentGlobals['gPlayerScriptExecutionContext'])
     } else {
-        showState('gScriptExecutionContextArray['+id+']', currentGlobals['gScriptExecutionContextArray'][id])
+        showState('gScriptExecutionContextArray[' + id + ']', currentGlobals['gScriptExecutionContextArray'][id])
     }
 }
 
@@ -139,8 +139,8 @@ function createRoom(parent, roomControls) {
 function createScroll(parent, roomControls) {
     let div = document.createElement('div');
     div.className = 'screen';
-    div.style.left = roomControls.roomScrollX-1 + 'px';
-    div.style.top = roomControls.roomScrollY-1 + 'px';
+    div.style.left = roomControls.roomScrollX - 1 + 'px';
+    div.style.top = roomControls.roomScrollY - 1 + 'px';
     div.style.width = '240px';
     div.style.height = '160px';
     parent.appendChild(div);
@@ -164,9 +164,9 @@ function createEntityLists(lists) {
         parent.appendChild(p);
         list.forEach((entity, entityIndex) => {
             let a = document.createElement('a');
-            a.onclick = () => {selectEntity(listIndex, entityIndex);};
+            a.onclick = () => { selectEntity(listIndex, entityIndex); };
             let name = getHumanName(entity);
-            a.innerHTML =  name[0] + ' ' + name[1];
+            a.innerHTML = name[0] + ' ' + name[1];
             parent.appendChild(a);
             parent.appendChild(document.createElement('br'));
         });
@@ -198,6 +198,9 @@ function addHeading(parent, text) {
     parent.appendChild(p);
 }
 
+
+let onlyShowCheckedFlags = true;
+
 function createFlags(flags, area) {
     var parent = document.getElementById('flags');
     parent.innerHTML = '';
@@ -209,24 +212,83 @@ function createFlags(flags, area) {
         checkbox.readOnly = true;
         checkbox.disabled = true;
         checkbox.checked = flags[i] == 1;
+        if (onlyShowCheckedFlags && !checkbox.checked) {
+            continue;
+        }
         parent.appendChild(checkbox);
         parent.appendChild(document.createTextNode(globalFlags[i]));
         parent.appendChild(document.createElement('br'));
     }
 
-    addHeading(parent, 'Local Flags (Offset: 0x' + area.localFlagOffset.toString(16) + ')');
-    for (let i = 0; i < 256; i++) { // TODO determine how many local flags there are in this area?
+    addUnnamedFlags(parent, flags, 0x0, globalFlags.length, 0x100);
+    addLocalFlags(parent, flags, 'Outdoors', 0x100, localFlagsOutdoors.length, localFlagsOutdoors, area);
+    addUnnamedFlags(parent, flags, 0x100, localFlagsOutdoors.length, 0x200);
+    addLocalFlags(parent, flags, 'Indoors', 0x200, localFlagsIndoors.length, localFlagsIndoors, area);
+    addUnnamedFlags(parent, flags, 0x200, localFlagsIndoors.length, 0x300);
+    addLocalFlags(parent, flags, 'Caves1', 0x300, localFlagsCaves1.length, localFlagsCaves1, area);
+    addUnnamedFlags(parent, flags, 0x300, localFlagsCaves1.length, 0x400);
+    addLocalFlags(parent, flags, 'Caves2', 0x400, localFlagsCaves2.length, localFlagsCaves2, area);
+    addUnnamedFlags(parent, flags, 0x400, localFlagsCaves2.length, 0x500);
+    addLocalFlags(parent, flags, 'DWS', 0x500, localFlagsDWS.length, localFlagsDWS, area);
+    addUnnamedFlags(parent, flags, 0x500, localFlagsDWS.length, 0x5C0);
+    addLocalFlags(parent, flags, 'COF', 0x5C0, localFlagsCOF.length, localFlagsCOF, area);
+    addUnnamedFlags(parent, flags, 0x5C0, localFlagsCOF.length, 0x680);
+    addLocalFlags(parent, flags, 'FOW', 0x680, localFlagsFOW.length, localFlagsFOW, area);
+    addUnnamedFlags(parent, flags, 0x680, localFlagsFOW.length, 0x740);
+    addLocalFlags(parent, flags, 'TOD', 0x740, localFlagsTOD.length, localFlagsTOD, area);
+    addUnnamedFlags(parent, flags, 0x740, localFlagsTOD.length, 0x800);
+    addLocalFlags(parent, flags, 'POW', 0x800, localFlagsPOW.length, localFlagsPOW, area);
+    addUnnamedFlags(parent, flags, 0x800, localFlagsPOW.length, 0x8C0);
+    addLocalFlags(parent, flags, 'DHC', 0x8C0, localFlagsDHC.length, localFlagsDHC, area);
+    addUnnamedFlags(parent, flags, 0x8C0, localFlagsDHC.length, 0xfff);
+    // addHeading(parent, 'Local Flags (Offset: 0x' + area.localFlagOffset.toString(16) + ')');
+    // for (let i = 0; i < 256; i++) { // TODO determine how many local flags there are in this area?
+    //     let checkbox = document.createElement('input');
+    //     checkbox.type = 'checkbox';
+    //     checkbox.readOnly = true;
+    //     checkbox.disabled = true;
+    //     checkbox.checked = flags[area.localFlagOffset+i] == 1;
+    //     parent.appendChild(checkbox);
+    //     parent.appendChild(document.createTextNode('0x' + i.toString(16)));
+    //     parent.appendChild(document.createElement('br'));
+    // }
+}
+
+function addLocalFlags(parent, flags, title, start, count, names, area) {
+    if (area.localFlagOffset == start) {
+        title = title + ' (current)'
+    }
+    addHeading(parent, title);
+    for (let i = 0; i < count; i++) { // TODO determine how many local flags there are in this area?
         let checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
         checkbox.readOnly = true;
         checkbox.disabled = true;
-        checkbox.checked = flags[area.localFlagOffset+i] == 1;
+        checkbox.checked = flags[start + i] == 1;
+        if (onlyShowCheckedFlags && !checkbox.checked) {
+            continue;
+        }
         parent.appendChild(checkbox);
-        parent.appendChild(document.createTextNode('0x' + i.toString(16)));
+        parent.appendChild(document.createTextNode(names[i]));
         parent.appendChild(document.createElement('br'));
     }
 }
 
+function addUnnamedFlags(parent, flags, start, count, end) {
+
+    for (let i = 0; i < end-start-count; i++) { // TODO determine how many local flags there are in this area?
+        if (flags[start +count+ i] == 1) {
+            let checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.readOnly = true;
+            checkbox.disabled = true;
+            checkbox.checked = true;
+            parent.appendChild(checkbox);
+            parent.appendChild(document.createTextNode('Unnamed: 0x'+start.toString(16)+' + 0x' + (count+i).toString(16)));
+            parent.appendChild(document.createElement('br'));
+        }
+    }
+}
 
 let currentLists = []
 let currentGlobals = {}
@@ -235,7 +297,7 @@ function showLists(roomControls, lists, globals) {
 
     // Clean up previous
     selectEntity(-1, -1);
-    for (let i = parent.childNodes.length -1; i >= 0; i--) {
+    for (let i = parent.childNodes.length - 1; i >= 0; i--) {
         let child = parent.childNodes[i];
         if (child.id != 'screenshot') {
             parent.removeChild(child);
@@ -245,7 +307,7 @@ function showLists(roomControls, lists, globals) {
 
     currentLists = lists
     currentGlobals = globals
-    parent.onclick = () => {selectEntity(-1, -1);}
+    parent.onclick = () => { selectEntity(-1, -1); }
     createEntityLists(lists);
     createRoom(parent, roomControls);
     createScroll(parent, roomControls);
